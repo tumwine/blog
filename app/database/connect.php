00596371943f5713f173493bc09c0412324abd97
tumwine <tumwineivan20@gmail.com>
2021-02-07 03:01:13 -0500

    Update connect.php

--- app/database/connect.php
+++ app/database/connect.php
@@ -2,7 +2,7 @@
 
 $host = "cis9cbtgerlk68wl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $user = "cmjqwx5buumfuuna";
-$pass = "s3xfui8th4q3t3vn";
+$pass = "ntrui9dqx2gdevds";
 $db_name = "ia61bdz6lm91hv34";
 
 $conn = new MySQLi($host,$user,$pass,$db_name);
