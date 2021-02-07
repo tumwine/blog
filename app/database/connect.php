7e310681d5bdebe0ad48b6a42bd06ab4f73bf1c5
tumwine <tumwineivan20@gmail.com>
2021-02-07 02:52:33 -0500

    Update connect.php

--- app/database/connect.php
+++ app/database/connect.php
@@ -3,7 +3,7 @@
 $host = "cis9cbtgerlk68wl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $user = "cmjqwx5buumfuuna";
 $pass = "s3xfui8th4q3t3vn";
-$db_name ="ia61bdz6lm91hv34";
+$db_name = "ia61bdz6lm91hv34";
 
 $conn = new MySQLi($host,$user,$pass,$db_name);
 
