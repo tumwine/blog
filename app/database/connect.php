c0bfd94f8180e43c459e58f6f5ecca516c3e36d9
tumwine <tumwineivan20@gmail.com>
2021-02-07 01:34:34 -0500

    Update connect.php

--- app/database/connect.php
+++ app/database/connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db_name ="blog";
+$host = "cis9cbtgerlk68wl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$user = "cmjqwx5buumfuuna";
+$pass = "s3xfui8th4q3t3vn";
+$db_name ="ia61bdz6lm91hv34";
 
 $conn = new MySQLi($host,$user,$pass,$db_name);
 
